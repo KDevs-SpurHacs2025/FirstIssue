3e8a7cafe96c130060f6d01cb368e4888ba6e7ab
hzkm613 <hzkm613@gmail.com>
2025-06-24 15:44:58 -0400

    Create Survey page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Survey from './pages/Survey'
 
 function App() {
-
   return (
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/survey" element={<Survey />} />
       </Routes>
   );
 }
