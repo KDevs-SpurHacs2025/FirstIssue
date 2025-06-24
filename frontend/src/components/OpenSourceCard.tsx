3aa15eeee32b657d4b53bca0f42d4e60fb9f4da8
hzkm613 <hzkm613@gmail.com>
2025-06-24 17:04:38 -0400

    Create Open Source Detail page

---
+++ frontend/src/components/OpenSourceCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const OpenSourceCard = () => {
+  return (
+    <div>
+      <h2>Open Source Project Title</h2>
+      <p>Short description of the open source project.</p>
+      <div>Technologies Used: React, Node.js</div>
+      <div>Contributors: User1, User2</div>
+    </div>
+  )
+}
+
+export default OpenSourceCard
